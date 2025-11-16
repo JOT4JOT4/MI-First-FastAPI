a4ad1ce06681001127cb244b75127bd8c9598939
JOT4JOT4 <you@example.com>
2025-11-16 13:25:58 -0300

    Permitir CORS con puerto LOCAL

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from app.api.v1.routes import router as api_router
+from fastapi.middleware.cors import CORSMiddleware
 from app.db.session import connect_to_mongo, close_mongo_connection, get_database
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup():
